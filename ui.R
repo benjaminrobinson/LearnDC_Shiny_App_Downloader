9478a855fdb0452fc85af827b5433e292bef69eb
benjaminrobinson <benj.robinson2@gmail.com>
2016-09-13 22:54:17 -0400

    add url link to github source

--- ui.R
+++ ui.R
@@ -12,9 +12,12 @@ shinyUI(fluidPage(
     downloadButton('downloadData', 'Download')
   ),
   mainPanel(
-    img(src='logo.png', align = "lower left")
+    img(src='logo.png', align = "lower left"),
+    h5("Link to Source Code:  ", a("https://github.com/benjaminrobinson/LearnDC_Shiny_App_Downloader", href="https://github.com/benjaminrobinson/LearnDC_Shiny_App_Downloader"))
   )
 ),
  h1(fluidRow(
     column(12, div(DT::dataTableOutput('x1',width='150%',height='150%'),style = "font-size: 45%; width: 25%")))
-	)))
+	   )
+  )
+)
